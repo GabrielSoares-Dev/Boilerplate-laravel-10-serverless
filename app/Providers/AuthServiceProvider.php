62f43b996e885afee0e3c327c94f909365465b0f
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-11-15 20:39:39 +0000

    remove comments

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,25 +2,15 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
+    protected $policies = [];
+
+
     public function boot(): void
     {
-        //
     }
 }
