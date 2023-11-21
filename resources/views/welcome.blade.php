9cdab580ffa25fd11c1c0ac9ade47f0da9f56901
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-11-20 21:22:28 -0300

    remove lock

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Boilerplate laravel 10</title>
-    </head>
-    <body>
-       <h1>Boilerplate laravel 10 test</h1>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Boilerplate laravel 10</title>
+</head>
+
+<body>
+    <h1>Boilerplate laravel 10</h1>
+</body>
+
 </html>
