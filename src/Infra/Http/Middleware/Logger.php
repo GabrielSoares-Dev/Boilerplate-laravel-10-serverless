244a10bd13943a8775378a3cc9d8c4331cde58b8
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-06-11 13:06:35 +0000

    feat(responde log): response log

--- src/Infra/Http/Middleware/Logger.php
+++ src/Infra/Http/Middleware/Logger.php
@@ -20,7 +20,7 @@ class Logger
         $response = $next($request);
 
         $this->loggerService->debug('request', (object) $request->all());
-        $this->loggerService->debug('response', $response);
+        $this->loggerService->debug('response', $response->original);
 
         return $response;
     }
